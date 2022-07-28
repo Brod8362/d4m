2a5d763b634d76adbbcc758a3dfca82217ce12ac
Brod8362 <brod8362@gmail.com>
2022-07-27 19:28:25 -0500

    Functional flatpak build

--- resources/export.sh
+++ resources/export.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 inkscape -w 128 -h 128 logo.svg -o EXPORT_logo.png
 inkscape -w 1280 -h 128 tagline.svg -o EXPORT_tagline.png
+sizes=(32 64 128)
+for size in "${sizes[@]}"
+do
+    echo "generating icon for $size px"
+    time inkscape -w "$size" -h "$size" ../resources/logo.svg -o "EXPORT_$size.png"
+done
 convert +append EXPORT_logo.png EXPORT_tagline.png EXPORT_github.png
 convert EXPORT_logo.png logo.ico
